e2d691002103d7d44f65ca1760ed7f58ce0aa159
Joe <jrf118@case.edu>
2016-10-23 19:22:11 -0400

    Initial commit with skeletons of our basic files

---
+++ Store.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _393_Food_Machine
+{
+    class Store
+    {
+    }
+}
